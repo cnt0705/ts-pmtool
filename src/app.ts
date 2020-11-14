a1ad2f91cec229ff96edcca3b86505d0409f5ad3
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-11-14 15:59:27 +0900

    Add gatherUserInput

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+function validate() {}
+
 function autobind(
   _target: any,
   _methodName: string,
@@ -51,10 +53,38 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enterdTitle = this.titleInputElement.value;
+    const enterdDescription = this.descriptionInputElement.value;
+    const enterdManday = this.mandayInputElement.value;
+
+    if (
+      validate({ value: enterdTitle, required: true, minLength: 5 }) &&
+      validate({ value: enterdDescription, required: true, minLength: 5 }) &&
+      validate({ value: enterdManday, required: true, minLength: 5 })
+    ) {
+      alert("入力値が不正です。");
+      return;
+    } else {
+      return [enterdTitle, enterdDescription, +enterdManday];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.mandayInputElement.value = "";
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, manday] = userInput;
+      console.log(title, desc, manday);
+      this.clearInputs();
+    }
   }
 
   private configure() {
